efb752d63e4adb53d3ceed4406c4b60b63248ad3
l-s-a-s-h-a-l <sasha.laigle@gmail.com>
2024-01-27 12:24:44 +0100

    Changement des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import { PresentationComponent } from './presentation/presentation.component';
 import { TarifsComponent } from './tarifs/tarifs.component';
 import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [
-  { path: '', component: PresentationComponent },
-  { path: '', component: TarifsComponent },
-  { path: '', component: ContactComponent },
-  { path: '', redirectTo: '/', pathMatch: 'full' }, // Redirige vers la présentation par défaut
-];
+// const routes: Routes = [
+//   { path: '', component: PresentationComponent },
+//   { path: '', component: TarifsComponent },
+//   { path: '', component: ContactComponent },
+//   { path: '', redirectTo: '/', pathMatch: 'full' }, // Redirige vers la présentation par défaut
+// ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
